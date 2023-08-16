f8dae0df4865e7be075d33160613c548140ce7d1
Joe Robinson <jrobinson@samaritan.com>
2023-08-15 22:17:45 -0600

    Add components directory

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import TheHeader from './components/TheHeader'
 
 export default function Home() {
   return (
     <main>
+      <TheHeader />
       <p>hello world</p>
     </main>
   )

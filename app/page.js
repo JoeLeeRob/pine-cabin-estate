ca08b8082b5acdf42da77ef60c560d66eb4a6839
Joe Robinson <jrobinson@samaritan.com>
2023-08-15 22:14:32 -0600

    Add tailwind css

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import NavHeader from './components/NavHeader'
 
 export default function Home() {
   return (
     <main>
-      <NavHeader />
       <p>hello world</p>
     </main>
   )
